347088f45e0450d95c39b0cc7dd726f92eec8b4f
jaaniles <jaani.leskinen@fraktio.fi>
2022-03-26 23:49:37 +0200

    centerize navigation

--- src/organisms/navigation/TopNavigation.tsx
+++ src/organisms/navigation/TopNavigation.tsx
@@ -2,9 +2,10 @@ import styled from "@emotion/styled";
 
 import { Row } from "~/atoms/Row";
 import { Link } from "~/atoms/typography/Link";
+import { scale } from "~/design";
 import { useUpdateDarkMode } from "~/providers/DarkModeProvider";
 
-const Container = styled.div({
+const Container = styled(Row)({
   background: "#C4C4C4C4",
   width: "100%",
 });
@@ -16,7 +17,10 @@ export const TopNavigation = () => {
 
   return (
     <Container>
-      <Row css={{ justifyContent: "space-between" }}>
+      <Row
+        css={{ width: "100%", maxWidth: scale(200), margin: "0 auto" }}
+        spaceBetween
+      >
         <Link href="/">Olutkerho </Link>
         <button onClick={handleToggleDarkMode}>darkmode</button>
       </Row>
