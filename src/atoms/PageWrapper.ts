ebbe17ef454020b5ea8de2d00653af6c8e97ada9
jaaniles <jaani.leskinen@fraktio.fi>
2022-03-26 22:13:36 +0200

    Add transition to darkmode switch

--- src/atoms/PageWrapper.ts
+++ src/atoms/PageWrapper.ts
@@ -4,4 +4,6 @@ export const PageWrapper = styled.div(({ theme }) => ({
   height: "100vh",
   color: theme.colors.text200,
   backgroundColor: theme.colors.background50,
+
+  transition: "background-color 1.25s",
 }));
