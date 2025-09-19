1af86718df13bcf287d21740804117f8e69ac52c
jaaniles <jaani.leskinen@fraktio.fi>
2025-09-19 17:19:43 +0300

    vibe

--- .eslintrc.js
+++ .eslintrc.js
@@ -93,7 +93,6 @@ module.exports = {
     ],
     "array-bracket-newline": ["error", "consistent"],
     "no-else-return": ["error", { allowElseIf: false }],
-    "import/no-cycle": [2, { maxDepth: 2, ignoreExternal: true }],
     "import/no-self-import": "error",
     "no-restricted-syntax": [
       "error",
